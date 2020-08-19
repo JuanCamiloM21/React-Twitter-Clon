ea07591611b7cd561322f5490fff6a2d54190eee
Juan Camilo Moreno <juancamilomg21@gmail.com>
2020-08-19 13:29:10 -0500

    Bug fixed

--- firebase.js
+++ firebase.js
@@ -1,13 +1,13 @@
 import firebase from 'firebase';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyAYa2TvG0NQvTX0_zKLfFqmabm2k3BfexQ',
-  authDomain: 'react-twitter-clon.firebaseapp.com',
-  databaseURL: 'https://react-twitter-clon.firebaseio.com',
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL,
   projectId: 'react-twitter-clon',
-  storageBucket: 'react-twitter-clon.appspot.com',
-  messagingSenderId: '1063181105715',
-  appId: '1:1063181105715:web:37a422cd39c3b4edccc146',
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
